f45e9e5269278d9c04bc707ba9cdffe38696611e
MikleM20 <jose.manila@ucalgary.ca>
2020-03-07 21:54:05 -0700

    Chat with no nickname, userList

---
+++ script.js
@@ -0,0 +1,44 @@
+$(function(){
+    var socket = io.connect();
+    var $messageform = $('#messageForm');
+    var $message = $('#message');
+    var $chat = $('#chat');
+    var $users =$('#users');
+    var $username = '';
+    var $window = $('html');
+    var $userList = ('#userList');
+    
+    
+
+    $messageform.submit(function(e){
+        e.preventDefault();
+        socket.emit('send message', $message.val());
+        $message.val('');
+    });
+
+
+
+    socket.on('new message', function(data){
+        $chat.append('<div class="well">'+data.msg+'</div>');
+    });
+
+    socket.on('get users', function(data){
+        var html='';
+        for(i = 0;i < data.length;i++){
+            html += '<li class="list-group-item">'+data[i]+'</li>';
+        }
+        $users.html(html);
+    });
+
+    window.onload= newUser();
+    
+    function newUser(){
+        $username = '' + Math.random().toString(36).substr(2, 9);
+        console.log('%s',$username);
+        socket.emit('new user', $username, function(data){
+        });
+        document.getElementById("who").innerHTML = "You are: " + $username;
+        $username='';
+    }
+
+});
---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('server ran');
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/style.css');
+});
+
+app.get('/script.js', function(req, res){
+    res.sendFile(__dirname + '/script.js');
+});
+
+io.sockets.on('connection', function(socket, callback){
+    connections.push(socket);
+    console.log("Connected: %s sockets connected", connections.length);    
+
+    //disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log("Disconnected: %s sockets connected", connections.length);
+    });
+
+    //Send Message
+    socket.on('send message', function(data){
+        
+        io.sockets.emit('new message', {msg: data});
+    });
+
+    //New User
+    socket.on('new user', function(data,callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+    
+});
